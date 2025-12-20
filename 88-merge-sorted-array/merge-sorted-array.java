71f1f3a4327e67923836b05b5f11ea8d8f0017f6
Rahul Pareek <162308724+rahulpareek011@users.noreply.github.com>
2025-12-20 21:24:58 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (15.81%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,31 +1,50 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] merge = new int[m+n];
-        int i=0;
-        int j=0;
-        int k=0;
-        while(i<m && j<n){
-            if(nums1[i]<nums2[j]){
-                merge[k++] = nums1[i];
-                i++;
+        int space = (m+n)-1;
+        while (n>0 && m>0) {
+            if(nums1[m-1]<=nums2[n-1]){
+                nums1[space] = nums2[n-1];
+                n--;
+                space--;
             }else{
-                merge[k++] = nums2[j];
-                j++;
+                nums1[space] = nums1[m-1];
+                space--;
+                m--;
             }
         }
-       while(i<m){
-        merge[k++] = nums1[i];
-        i++;
-       }
+        int j=0;
+        int k=0;
+        while(j<n){
+            nums1[k++] = nums2[j];
+            j++;
+        }
 
-       while(j<n){
-        merge[k++] = nums2[j];
-        j++;
-       }
+    //     int[] merge = new int[m+n];
+    //     int i=0;
+    //     int j=0;
+    //     int k=0;
+    //     while(i<m && j<n){
+    //         if(nums1[i]<nums2[j]){
+    //             merge[k++] = nums1[i];
+    //             i++;
+    //         }else{
+    //             merge[k++] = nums2[j];
+    //             j++;
+    //         }
+    //     }
+    //    while(i<m){
+    //     merge[k++] = nums1[i];
+    //     i++;
+    //    }
 
-       for(int b = 0;b<nums1.length;b++){
-        nums1[b] = merge[b];
-       }
+    //    while(j<n){
+    //     merge[k++] = nums2[j];
+    //     j++;
+    //    }
+
+    //    for(int b = 0;b<nums1.length;b++){
+    //     nums1[b] = merge[b];
+    //    }
         
         // int k=(n+m)-1;
         // for(int i=nums2.length-1;i>0;i--){
