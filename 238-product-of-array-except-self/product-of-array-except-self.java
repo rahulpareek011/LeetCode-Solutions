20a0bd5823556dd38d55ddb68d374ae9bc00ce1c
Rahul Pareek <162308724+rahulpareek011@users.noreply.github.com>
2025-12-22 00:45:25 +0530

    Time: 3 ms (26.38%) | Memory: 65 MB (60.64%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        //Finding Prefix Product and Suffix Product
+        int n = nums.length;
+        int pre[] = new int[n];
+        int suff[] = new int[n];
+        pre[0] = 1;
+        suff[n - 1] = 1;
+        
+        for(int i = 1; i < n; i++) {
+            pre[i] = pre[i - 1] * nums[i - 1];
+        }
+        for(int i = n - 2; i >= 0; i--) {
+            suff[i] = suff[i + 1] * nums[i + 1];
+        }
+        
+        int ans[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            ans[i] = pre[i] * suff[i];
+        }
+        return ans;
+
+        //brute force
+        // int ans[] = new int[nums.length];
+        // for(int i=0;i<nums.length;i++){
+        //     int sum = 1;
+        //     for(int j=0;j<nums.length;j++){
+        //         if(i!=j){
+        //             sum = sum*nums[j];
+        //         }
+        //     }
+        //     ans[i] = sum;
+        // }
+        // return ans;
+    }
+}
