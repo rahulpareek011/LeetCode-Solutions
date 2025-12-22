9aec4f00ae4b6e3cd462350d469b3b536a46fab9
Rahul Pareek <162308724+rahulpareek011@users.noreply.github.com>
2025-12-22 20:54:26 +0530

    Time: 2 ms (92.90%) | Memory: 64.2 MB (78.69%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,24 +1,44 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        //Finding Prefix Product and Suffix Product
         int n = nums.length;
         int pre[] = new int[n];
-        int suff[] = new int[n];
-        pre[0] = 1;
-        suff[n - 1] = 1;
+        pre[0] = nums[0];
+        for(int i=1;i<n;i++){
+            pre[i] = pre[i-1] * nums[i];
+        }
+        int res[] = new int[n];
+        res[n-1] = pre[n-2];
+        int prod = nums[n-1];//suffix
+        for(int i=n-2;i>=0;i--){
+            if(i>0){
+                res[i] = pre[i-1]*prod;//multiplying prefix and suffix
+            } else{
+                res[i] = prod;
+            }
+            prod = prod * nums[i]; //increasing suffix
+        }
+        return res;
         
-        for(int i = 1; i < n; i++) {
-            pre[i] = pre[i - 1] * nums[i - 1];
-        }
-        for(int i = n - 2; i >= 0; i--) {
-            suff[i] = suff[i + 1] * nums[i + 1];
-        }
+
+        // //Finding Prefix Product and Suffix Product
+        // int n = nums.length;
+        // int pre[] = new int[n];
+        // int suff[] = new int[n];
+        // pre[0] = 1;
+        // suff[n - 1] = 1;
         
-        int ans[] = new int[n];
-        for(int i = 0; i < n; i++) {
-            ans[i] = pre[i] * suff[i];
-        }
-        return ans;
+        // for(int i = 1; i < n; i++) {
+        //     pre[i] = pre[i - 1] * nums[i - 1];
+        // }
+        // for(int i = n - 2; i >= 0; i--) {
+        //     suff[i] = suff[i + 1] * nums[i + 1];
+        // }
+        
+        // int ans[] = new int[n];
+        // for(int i = 0; i < n; i++) {
+        //     ans[i] = pre[i] * suff[i];
+        // }
+        // return ans;
 
         //brute force
         // int ans[] = new int[nums.length];
