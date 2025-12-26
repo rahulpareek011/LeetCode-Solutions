1694d1355ad465659d21eb028818d5caef982fd1
Rahul Pareek <162308724+rahulpareek011@users.noreply.github.com>
2025-12-26 16:58:50 +0530

    Time: 1 ms (99.79%) | Memory: 77.4 MB (25.08%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,20 +1,36 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         int sum = 0;
-        int prefixSum[] = new int[nums.length];
+        int maxi = Integer.MIN_VALUE;
 
-        for(int i = 0; i < nums.length; i++){
-            sum += nums[i];
-            prefixSum[i] = sum;
+        for(int i=0;i<nums.length;i++){
+            sum = sum+nums[i];
+
+            if(sum>maxi){
+                maxi = sum;
+            }
+            if(sum<0){
+                sum = 0;
+            }
         }
+        return maxi;
+
+
+        // int sum = 0;
+        // int prefixSum[] = new int[nums.length];
+
+        // for(int i = 0; i < nums.length; i++){
+        //     sum += nums[i];
+        //     prefixSum[i] = sum;
+        // }
         
-        int minPrefix = 0;
-        int maxSum = Integer.MIN_VALUE;
+        // int minPrefix = 0;
+        // int maxSum = Integer.MIN_VALUE;
 
-        for(int i = 0; i < nums.length; i++){
-            maxSum = Math.max(maxSum, prefixSum[i] - minPrefix);
-            minPrefix = Math.min(minPrefix, prefixSum[i]);
-        }
-        return maxSum;
+        // for(int i = 0; i < nums.length; i++){
+        //     maxSum = Math.max(maxSum, prefixSum[i] - minPrefix);
+        //     minPrefix = Math.min(minPrefix, prefixSum[i]);
+        // }
+        // return maxSum;
     }
 }
