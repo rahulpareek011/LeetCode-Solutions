0252c31d98693a62de79d763e96f31cf561f68e5
Rahul Pareek <162308724+rahulpareek011@users.noreply.github.com>
2025-12-29 23:27:20 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (36.82%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,33 +1,62 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int num1 = 0;
-        int num2 = 0;
-        int num3 = 0;
+        /*
+         “I maintain three pointers: one for 0s, one for 2s,
+          and one current pointer. Based on the current value,
+          I swap and shrink boundaries while maintaining the invariant.”
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                num1++;
+        🧠 Mental sentence to remember forever
+        “I don’t sort elements — I only move boundaries until nothing is unknown.”
+        */
+        int i= 0;
+        int j = 0;
+        int k = nums.length-1;
+        while(j<=k){
+            if(nums[j]==0){
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                i++;
+                j++;
             }
-            if(nums[i]==1){
-                num2++;
+            else if(nums[j]==2){
+                int temp = nums[j];
+                nums[j] = nums[k];
+                nums[k] = temp;
+                k--;
+            }else{
+                j++;
             }
-            if(nums[i]==2){
-                num3++;
-            }
         }
-        int k = 0;
-        for(int i=0;i<num1;i++){
-            nums[k] = 0;
-            k++;
-        }
-        for(int i=0;i<num2;i++){
-            nums[k] = 1;
-            k++;
-        }
-        for(int i=0;i<num3;i++){
-            nums[k] = 2;
-            k++;
-        }
+
+        // int num1 = 0;
+        // int num2 = 0;
+        // int num3 = 0;
+
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==0){
+        //         num1++;
+        //     }
+        //     if(nums[i]==1){
+        //         num2++;
+        //     }
+        //     if(nums[i]==2){
+        //         num3++;
+        //     }
+        // }
+        // int k = 0;
+        // for(int i=0;i<num1;i++){
+        //     nums[k] = 0;
+        //     k++;
+        // }
+        // for(int i=0;i<num2;i++){
+        //     nums[k] = 1;
+        //     k++;
+        // }
+        // for(int i=0;i<num3;i++){
+        //     nums[k] = 2;
+        //     k++;
+        // }
 
         // for(int i=0;i<nums.length;i++){
         //     for(int j=i+1;j<nums.length;j++){
