9fdc9c47e11ef224c325daa6b72ccebfbdd3c99d
Rahul Pareek <162308724+rahulpareek011@users.noreply.github.com>
2025-12-31 10:26:24 +0530

    Time: 5 ms (57.93%) | Memory: 47.1 MB (23.20%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String convert(String s, int numRows) {
+       if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+
+        List<Character>[] rows = new ArrayList[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new ArrayList<>();
+        }
+
+        int idx = 0, d = 1;
+        for (char c : s.toCharArray()) {
+            rows[idx].add(c);
+            if (idx == 0) {
+                d = 1;
+            } else if (idx == numRows - 1) {
+                d = -1;
+            }
+            idx += d;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (List<Character> row : rows) {
+            for (char c : row) {
+                result.append(c);
+            }
+        }
+
+        return result.toString();        
+    }
+}
+
+/*
+    🔁 Why this works:
+    Condition	            Action	    Reason
+    idx == 0	            d = 1       You hit top, must go down
+    idx == numRows - 1	    d = -1	    You hit bottom, must go up
+*/
