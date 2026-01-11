ae7499e4fc109bb91a72eb48438343bf3ecfd541
Rahul Pareek <162308724+rahulpareek011@users.noreply.github.com>
2026-01-11 16:36:53 +0530

    Time: 2 ms (99.37%) | Memory: 48.2 MB (60.94%) - LeetSync

---
+++ 495-teemo-attacking/teemo-attacking.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int start = timeSeries[0];
+        int end = start+duration-1;
+        int totDay = 0;
+        for(int i=0;i<timeSeries.length;i++){
+            if(end<timeSeries[i]){
+                totDay= totDay+end-start+1;
+                start = timeSeries[i];
+                end = start+duration-1; 
+            }else{
+                end = timeSeries[i]+duration-1;
+            }
+        }
+        totDay+=(end-start+1);
+        return totDay;
+    }
+}
