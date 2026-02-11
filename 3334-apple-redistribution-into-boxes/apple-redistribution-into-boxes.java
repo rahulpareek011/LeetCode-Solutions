272e6363e88d4c3105f730acf10adf138758c49a
Rahul Pareek <162308724+rahulpareek011@users.noreply.github.com>
2026-02-11 23:16:11 +0530

    Time: 5 ms (21.16%) | Memory: 44.4 MB (61.97%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.java
@@ -6,8 +6,6 @@ class Solution {
         for(int i=0;i<apple.length;i++){
             tot+=apple[i];
         }
-        System.out.println(tot);
-        System.out.println(Arrays.toString(capacity));
         int sum = 0;
         for(int i =capacity.length-1;i>=0;i--){
             if(sum<tot){
