672fbb81c31081d64ba81bcc1def008220aec9e3
Rahul Pareek <162308724+rahulpareek011@users.noreply.github.com>
2026-03-30 23:24:03 +0530

    Time: 3 ms (26.44%) | Memory: 64.9 MB (84.22%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,23 +1,41 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int pre[] = new int[n];
-        pre[0] = nums[0];
-        for(int i=1;i<n;i++){
-            pre[i] = pre[i-1] * nums[i];
+        int ans[] = new int[nums.length];
+        int[] prefix = new int[nums.length];
+        int[] suffix = new int[nums.length];
+        prefix[0] = nums[0];
+        for(int i=1;i<nums.length;i++){
+            prefix[i] = prefix[i-1]*nums[i];
         }
-        int res[] = new int[n];
-        res[n-1] = pre[n-2];
-        int prod = nums[n-1];//suffix
-        for(int i=n-2;i>=0;i--){
-            if(i>0){
-                res[i] = pre[i-1]*prod;//multiplying prefix and suffix
-            } else{
-                res[i] = prod;
-            }
-            prod = prod * nums[i]; //increasing suffix
+        suffix[nums.length-1] = nums[nums.length-1];
+        for(int i=nums.length-1;i>0;i--){
+            suffix[i-1] = suffix[i]*nums[i-1];
         }
-        return res;
+        ans[0] = suffix[1];
+        ans[nums.length-1] = prefix[nums.length-2];
+        for(int i =1;i<nums.length-1;i++){
+            ans[i] = prefix[i-1]*suffix[i+1];
+        }
+        return ans;
+        
+        // int n = nums.length;
+        // int pre[] = new int[n];
+        // pre[0] = nums[0];
+        // for(int i=1;i<n;i++){
+        //     pre[i] = pre[i-1] * nums[i];
+        // }
+        // int res[] = new int[n];
+        // res[n-1] = pre[n-2];
+        // int prod = nums[n-1];//suffix
+        // for(int i=n-2;i>=0;i--){
+        //     if(i>0){
+        //         res[i] = pre[i-1]*prod;//multiplying prefix and suffix
+        //     } else{
+        //         res[i] = prod;
+        //     }
+        //     prod = prod * nums[i]; //increasing suffix
+        // }
+        // return res;
         
 
         // //Finding Prefix Product and Suffix Product
