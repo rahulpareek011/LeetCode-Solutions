36c013f119dd08b7057f424d61eee61670d78bc6
Rahul Pareek <162308724+rahulpareek011@users.noreply.github.com>
2026-04-16 22:40:13 +0530

    Time: 1 ms (99.84%) | Memory: 55.8 MB (36.98%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,18 +1,61 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int count = 0;
-        int probAns = nums[0];
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==probAns){
+        int p = nums[0];
+        int count = 1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==p){
                 count++;
             } else{
                 count--;
             }
-            if(count == 0){
-                probAns = nums[i];
+            if(count==0){
+                p = nums[i];
                 count = 1;
             }
         }
-        return probAns;
+        return p;
+
+
+        
+        
+        
+        
+        
+        
+        
+        // int candidate = nums[0];
+        // int count = 1;
+
+        // for(int i=1;i<nums.length;i++){
+        //     if(candidate == nums[i]){
+        //         count++;
+        //     }else{
+        //         count--;
+        //     }
+        //     if(count==0){
+        //         candidate = nums[i];
+        //         count = 1;
+        //     }
+        // }
+        // return candidate;
+        // Arrays.sort(nums);
+        // return nums[nums.length]/2;
+
+        //boyle moore voting algo
+        //time complexity = 1
+        // int count = 1;
+        // int probAns = nums[0];
+        // for(int i=1;i<nums.length;i++){
+        //     if(nums[i]==probAns){
+        //         count++;
+        //     } else{
+        //         count--;
+        //     }
+        //     if(count == 0){
+        //         probAns = nums[i];
+        //         count = 1;
+        //     }
+        // }
+        // return probAns;
     }
 }
