33bbfbaeee55b22cbf2c491d98bd7009d6b89705
Rahul Pareek <162308724+rahulpareek011@users.noreply.github.com>
2026-04-17 23:05:03 +0530

    Time: 142 ms (7.93%) | Memory: 49.8 MB (5.20%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,23 +1,42 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int[] ans = new int[2];
-        //binary search (O(Log n))
-        int strt = 0;
-        int end = numbers.length-1;
-        while(strt<end){
-            int sum = numbers[strt]+numbers[end];
-            if(sum==target){
-                ans[0] = strt+1;
-                ans[1] = end+1;
-                return ans;
-            }
-            else if(sum>target){
-                end--;
-            }else{
-                strt++;
+        int j = numbers.length-1;
+        int i = 0;
+        while(i<j){
+            int n = numbers[i] + numbers[j];
+            System.out.println(n);
+            if(n==target){
+                return new int[]{i+1,j+1};
+            } else if(n>target){
+                j--;
+            } else{
+                i++;
             }
         }
-        return ans;
+        return new int[]{0,0};
+
+
+
+
+
+
+        // int[] ans = new int[2];
+        // int strt = 0;
+        // int end = numbers.length-1;
+        // while(strt<end){
+        //     int sum = numbers[strt]+numbers[end];
+        //     if(sum==target){
+        //         ans[0] = strt+1;
+        //         ans[1] = end+1;
+        //         return ans;
+        //     }
+        //     else if(sum>target){
+        //         end--;
+        //     }else{
+        //         strt++;
+        //     }
+        // }
+        // return ans;
         
         //brute force
         // for(int i=0;i<numbers.length;i++){
