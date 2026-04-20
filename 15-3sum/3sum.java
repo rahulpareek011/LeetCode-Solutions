22204a6bfae39fdcd95b72f08a8d30bc3323d472
Rahul Pareek <162308724+rahulpareek011@users.noreply.github.com>
2026-04-20 23:04:14 +0530

    Time: 33 ms (73.79%) | Memory: 59.1 MB (76.80%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,32 +1,52 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // Store unique triplets
-        Set<List<Integer>> ans = new HashSet<>();
-        int n= nums.length;
-        // First loop for first element
-        for (int i = 0; i < n; i++) {
-            // Set to store elements seen in this iteration
-            Set<Integer> hashset = new HashSet<>();
-
-            // Second loop for second element
-            for (int j = i + 1; j < n; j++) {
-                // Calculate third element needed
-                int third = -(nums[i] + nums[j]);
-
-                // If third already in set, we found a triplet
-                if (hashset.contains(third)) {
-                    List<Integer> temp = Arrays.asList(nums[i], nums[j], third);
-                    Collections.sort(temp);
-                    ans.add(temp);
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++){
+            while(i > 0 && nums[i] == nums[i -1] && i < nums.length - 2) i++;
+            int target = -1 * nums[i];
+            int j = i+1;
+            int k = nums.length-1;
+            while(j<k){
+                int sum = nums[j] + nums[k];
+                if(sum==target){
+                    List<Integer> ls = Arrays.asList(nums[i],nums[j],nums[k]);
+                    res.add(ls);
+                    j++;
+                    k--;
+                    while(j<nums.length && nums[j]==nums[j-1]){
+                        j++;
+                    }
+                    while(k>=0 && nums[k]==nums[k+1]){
+                        k--;
+                    }
+                } else if(sum<target){
+                    j++;
+                } else{
+                    k--;
                 }
-
-                // Add current element to set
-                hashset.add(nums[j]);
             }
         }
+        return res;
 
-        // Convert set to list
-        return new ArrayList<>(ans);
+
+
+
+        // Set<List<Integer>> ans = new HashSet<>();
+        // int n = nums.length;
+        // for(int i=0;i<n;i++){
+        //     Set<Integer> hs = new HashSet<>();
+        //     for(int j=i+1;j<n;j++){
+        //         int third = -(nums[i]+nums[j]);
+        //         if(hs.contains(third)){
+        //             List<Integer> temp = Arrays.asList(nums[i],nums[j],third);
+        //             Collections.sort(temp);
+        //             ans.add(temp);
+        //         }
+        //         hs.add(nums[j]);
+        //     }
+        // }
+        // return new ArrayList<>(ans);
 
 
 
