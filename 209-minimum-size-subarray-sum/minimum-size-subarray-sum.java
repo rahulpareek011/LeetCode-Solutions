a4e55ebda11af426d19804d792523d5564a675a5
Rahul Pareek <162308724+rahulpareek011@users.noreply.github.com>
2026-04-24 23:46:16 +0530

    Time: 1 ms (99.94%) | Memory: 62.3 MB (17.39%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,38 +1,57 @@
 class Solution {
-    private boolean check(int[] nums, int len, int t) {
-        int sum = 0;
+    // private boolean check(int[] nums, int len, int t) {
+    //     int sum = 0;
+    //     for (int i = 0; i < nums.length; i++) {
+    //         sum += nums[i];
 
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
+    //         if (i >= len)
+    //             sum -= nums[i - len];
 
-            if (i >= len)
-                sum -= nums[i - len];
-
-            if (i >= len - 1 && sum >= t)
-                return true;
-        }
-        return false;
-    }
+    //         if (i >= len - 1 && sum >= t)
+    //             return true;
+    //     }
+    //     return false;
+    // }
 
     public int minSubArrayLen(int target, int[] nums) {
-            int n=nums.length;
-            int s=1;
-            int e=n;
-            int ans=n+1;
-            while(s<=e)
-            {
-                int mid=s+(e-s)/2;
-                if(check(nums,mid,target))
-                {
-                    ans=mid;
-                    e=mid-1;
-                }
-                else
-                    s=mid+1;
+        int left = 0;
+        int high = 0;
+        int ans = Integer.MAX_VALUE;
+
+        int sum = 0;
+        while(high<nums.length){
+            sum+=nums[high];
+            while(sum>=target){
+                ans = Math.min(ans,(high-left)+1);
+                sum-=nums[left++];
             }
-            if(ans==n+1)
-                return 0;
+            high++;
+        }
+        if(left==0){
+            return 0;
+        }else{
             return ans;
+        }
+
+
+            // int n=nums.length;
+            // int s=1;
+            // int e=n;
+            // int ans=n+1;
+            // while(s<=e)
+            // {
+            //     int mid=s+(e-s)/2;
+            //     if(check(nums,mid,target))
+            //     {
+            //         ans=mid;
+            //         e=mid-1;
+            //     }
+            //     else
+            //         s=mid+1;
+            // }
+            // if(ans==n+1)
+            //     return 0;
+            // return ans;
 
 
         // //sliding window ( O(n) + O(1) )
@@ -50,18 +69,18 @@ class Solution {
         // }
         // if(len>0 && len!=Integer.MAX_VALUE) return len;
         // return 0;
-        /*
-        “Since all elements are positive, I can use a sliding window.
-        I expand the window by moving the right pointer and adding to the sum.
-        When the sum becomes greater than or equal to the target, I try to shrink 
-        the window from the   left to minimize the length while keeping the sum valid.
-        Each element is added and removed at most once, so the time complexity is O(n).”
+        // /*
+        // “Since all elements are positive, I can use a sliding window.
+        // I expand the window by moving the right pointer and adding to the sum.
+        // When the sum becomes greater than or equal to the target, I try to shrink 
+        // the window from the   left to minimize the length while keeping the sum valid.
+        // Each element is added and removed at most once, so the time complexity is O(n).”
 
-        When negatives are allowed, we switch patterns:
-        ✅ Prefix Sum + HashMap
+        // When negatives are allowed, we switch patterns:
+        // ✅ Prefix Sum + HashMap
 
-        Example:
-        LeetCode 560 (Subarray Sum Equals K)
-        */
+        // Example:
+        // LeetCode 560 (Subarray Sum Equals K)
+        // */
     }
 }
