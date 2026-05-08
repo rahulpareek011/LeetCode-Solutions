9e60898b2541ab1d8e84118e4fd6e778b423fb71
Rahul Pareek <162308724+rahulpareek011@users.noreply.github.com>
2026-05-08 23:26:06 +0530

    Time: 18 ms (41.77%) | Memory: 45.7 MB (14.44%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,21 +1,23 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        int maxdiff = Integer.MAX_VALUE;
+        int diff = Integer.MAX_VALUE;
         int ans = 0;
-        for(int i = 0;i<nums.length-2;i++){
+        for(int i=0;i<nums.length-2;i++){
+            while(i>0 && nums[i]==nums[i-1] && i<nums.length-2) i++;
             int j = i+1;
             int k = nums.length-1;
+
             while(j<k){
-                int sum = nums[i] + nums[j] + nums[k];
-                if(Math.abs(target - sum) < maxdiff){
-                    maxdiff = Math.abs(target-sum);
+                int sum = nums[i]+nums[j]+nums[k];
+                
+                if(Math.abs(sum-target)<diff){
+                    diff = Math.abs(sum-target);
                     ans = sum;
                 }
                 if(sum==target){
                     return ans;
-                } else if(sum>target){
+                }else if(sum>target){
                     k--;
                 } else{
                     j++;
@@ -23,5 +25,31 @@ class Solution {
             }
         }
         return ans;
+        
+        
+        
+        
+        // Arrays.sort(nums);
+        // int maxdiff = Integer.MAX_VALUE;
+        // int ans = 0;
+        // for(int i = 0;i<nums.length-2;i++){
+        //     int j = i+1;
+        //     int k = nums.length-1;
+        //     while(j<k){
+        //         int sum = nums[i] + nums[j] + nums[k];
+        //         if(Math.abs(target - sum) < maxdiff){
+        //             maxdiff = Math.abs(target-sum);
+        //             ans = sum;
+        //         }
+        //         if(sum==target){
+        //             return ans;
+        //         } else if(sum>target){
+        //             k--;
+        //         } else{
+        //             j++;
+        //         }
+        //     }
+        // }
+        // return ans;
     }
 }
