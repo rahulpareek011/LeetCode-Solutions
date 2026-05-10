ce891f4d8ed2ddc64feb69ebb97a9562bf9bf7df
Rahul Pareek <162308724+rahulpareek011@users.noreply.github.com>
2026-05-10 22:45:54 +0530

    Time: 7 ms (36.50%) | Memory: 46.1 MB (58.04%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,9 +1,9 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Map<Character,Integer> hs = new HashMap<>();
-        int low = 0;
+        int left = 0;
         int ans = 0;
-        for(int high= 0;high<s.length();high++){
+        Map<Character,Integer> hs = new HashMap<>();
+        for(int high = 0;high<s.length();high++){
             char ch = s.charAt(high);
             if(hs.containsKey(ch)){
                 int val = hs.get(ch);
@@ -12,19 +12,51 @@ class Solution {
                 hs.put(ch,1);
             }
 
-            while(hs.size()<high-low+1){
-                int value = hs.get(s.charAt(low));
-                value--;
-                if(value==0){
-                    hs.remove(s.charAt(low));
-                } else{
-                    hs.put(s.charAt(low),value);
+            while(high-left+1>hs.size()){
+                int lv = hs.get(s.charAt(left));
+                if(lv==1){
+                    hs.remove(s.charAt(left));
+                }else{
+                    hs.put(s.charAt(left),--lv);
                 }
-                low++;
+                left++;
             }
-
-            ans = Math.max(ans,high-low+1);
+            ans = Math.max(ans,high-left+1);
         }
         return ans;
+        
+        
+        
+        
+        
+        
+        
+        
+        // Map<Character,Integer> hs = new HashMap<>();
+        // int low = 0;
+        // int ans = 0;
+        // for(int high= 0;high<s.length();high++){
+        //     char ch = s.charAt(high);
+        //     if(hs.containsKey(ch)){
+        //         int val = hs.get(ch);
+        //         hs.put(ch,++val);
+        //     }else{
+        //         hs.put(ch,1);
+        //     }
+
+        //     while(hs.size()<high-low+1){
+        //         int value = hs.get(s.charAt(low));
+        //         value--;
+        //         if(value==0){
+        //             hs.remove(s.charAt(low));
+        //         } else{
+        //             hs.put(s.charAt(low),value);
+        //         }
+        //         low++;
+        //     }
+
+        //     ans = Math.max(ans,high-low+1);
+        // }
+        // return ans;
     }
 }
