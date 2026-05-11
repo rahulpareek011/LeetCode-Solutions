7b91730dc4cf9df5d80b2161a2343f940cc00f13
Rahul Pareek <162308724+rahulpareek011@users.noreply.github.com>
2026-05-11 23:49:12 +0530

    Time: 99 ms (5.03%) | Memory: 46.4 MB (30.99%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,32 +1,53 @@
 class Solution {
-    public int maxFreq(int[] arr){
+    public int maxF(int[] arr){
         int ans = 0;
         for(int n:arr){
-            ans = Math.max(n,ans);
+            ans = Math.max(ans,n);
         }
         return ans;
     }
+    
     public int characterReplacement(String s, int k) {
         int low = 0;
-        int maxF = 0;
-        int n = s.length();
-        int[] arr = new int[256];
+        int maxf = 0;
         int ans = 0;
+        int arr[] = new int[256];
 
-        for(int high=0;high<n;high++){
+        for(int high = 0;high<s.length();high++){
             arr[s.charAt(high)]++;
-            System.out.println(arr[s.charAt(high)]);
 
-            maxF = maxFreq(arr);
-            System.out.println(maxF);
-
-            while((high-low+1)-maxF>k){
+            maxf = maxF(arr);
+            while((high-low+1)-maxf>k){
                 arr[s.charAt(low)]--;
                 low++;
-                maxF = maxFreq(arr);
+                maxf = maxF(arr);
             }
             ans = Math.max(high-low+1,ans);
         }
         return ans;
+
+
+
+
+
+    //     int low = 0;
+    //     int maxF = 0;
+    //     int n = s.length();
+    //     int[] arr = new int[256];
+    //     int ans = 0;
+
+    //     for(int high=0;high<n;high++){
+    //         arr[s.charAt(high)]++;
+
+    //         maxF = maxFreq(arr);
+
+    //         while((high-low+1)-maxF>k){
+    //             arr[s.charAt(low)]--;
+    //             low++;
+    //             maxF = maxFreq(arr);
+    //         }
+    //         ans = Math.max(high-low+1,ans);
+    //     }
+    //     return ans;
     }
 }
